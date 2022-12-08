a12ab63b62f097091b53279d75685e711e748f52
boatrainlsz <yinger521999@gmail.com>
2022-12-08 21:55:00 +0800

    new commit: Thu 08 Dec 2022 09:55:00 PM CST

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /mnt/d/research/self-cultivation-of-programmer
+# Install script for directory: /home/boatrain/research/self-cultivation-of-programmer
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/mnt/d/research/self-cultivation-of-programmer/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/boatrain/research/self-cultivation-of-programmer/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
